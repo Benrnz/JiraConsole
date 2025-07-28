fd0e9cd4ef2d666e128367178bb05e0bdc33f963
Ben Rees <ben@rees.biz>
2025-07-28 21:48:14 +1200

    Template for secrets file

---
+++ Secrets.rename-me.cs
@@ -0,0 +1,8 @@
+﻿//namespace BensJiraConsole;
+// Use this template to create your own Secrets.cs file containing your username and token.
+// DO NOT CHECK THIS FILE INTO SOURCE CONTROL.
+// public static class Secrets
+// {
+//     public const string Username = "your-email@domain.com";
+//     public const string JiraToken = "Your-api-token-from-Jira";
+// }
