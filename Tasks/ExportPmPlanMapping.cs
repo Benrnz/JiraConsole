ec5b70a0b466911b1a5a67f6390fa25ce5695f4e
Ben Rees <ben@rees.biz>
2025-08-13 08:17:22 +1200

    Add display name to PMPlan mapping

--- Tasks/ExportPmPlanMapping.cs
+++ Tasks/ExportPmPlanMapping.cs
@@ -15,6 +15,7 @@ public class ExportPmPlanMapping : IJiraExportTask
         new("customfield_10004", "StoryPoints"),
         new("timeoriginalestimate", "Original Estimate"),
         new("created"),
+        new("reporter", "Reporter", "displayName"),
     ];
     public FieldMapping[] PmPlanFields =>
     [
