abeed8c5f0c23530388b4ffd6a3c7fd185334ddf
Ben Rees <ben@rees.biz>
2025-08-25 16:28:43 +1200

    Automate my weekly reports

---
+++ WeeklyReports.bat
@@ -0,0 +1,10 @@
+C:
+cd \Development\BensJiraConsole\Bin\Debug\net9.0
+BensJiraConsole.exe BUG_STATS
+BensJiraConsole.exe NOESTIMATE
+BensJiraConsole.exe PMPLAN_BURNUP
+BensJiraConsole.exe PMPLAN_NEW
+BensJiraConsole.exe PMPLAN_RBURNUP
+BensJiraConsole.exe PMPLAN_STORIES
+BensJiraConsole.exe PMPLANS
+BensJiraConsole.exe WB_BURNUP
