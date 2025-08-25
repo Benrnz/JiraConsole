42b30f0cf327d917a25f2b421306ab7080b01ac3
Ben Rees <ben@rees.biz>
2025-08-25 16:31:02 +1200

    Reorder weekly reports

--- WeeklyReports.bat
+++ WeeklyReports.bat
@@ -3,8 +3,8 @@ cd \Development\BensJiraConsole\Bin\Debug\net9.0
 BensJiraConsole.exe BUG_STATS
 BensJiraConsole.exe NOESTIMATE
 BensJiraConsole.exe PMPLAN_BURNUP
-BensJiraConsole.exe PMPLAN_NEW
 BensJiraConsole.exe PMPLAN_RBURNUP
 BensJiraConsole.exe PMPLAN_STORIES
 BensJiraConsole.exe PMPLANS
 BensJiraConsole.exe WB_BURNUP
+BensJiraConsole.exe PMPLAN_NEW
