570b1b6d55eee3e21dbffb166c53ed41d2727754
Ben Rees <ben@rees.biz>
2025-08-29 08:56:54 +1200

    Include start and finish date in the log file

--- WeeklyReports.bat
+++ WeeklyReports.bat
@@ -1,10 +1,17 @@
-C:
+cls
+@ECHO OFF
+ECHO ==================================================================================== >> BensJiraConsole.log
+ECHO Start time: %date% %time%
+ECHO Start time: %date% %time% >> BensJiraConsole.log
 cd \Development\BensJiraConsole\Bin\Debug\net9.0
-BensJiraConsole.exe BUG_STATS
-BensJiraConsole.exe NOESTIMATE
-BensJiraConsole.exe PMPLAN_BURNUP
-BensJiraConsole.exe PMPLAN_RBURNUP
-BensJiraConsole.exe PMPLAN_STORIES
-BensJiraConsole.exe PMPLANS
-BensJiraConsole.exe WB_BURNUP
-BensJiraConsole.exe PMPLAN_NEW
+BensJiraConsole.exe BUG_STATS >> BensJiraConsole.log
+BensJiraConsole.exe NOESTIMATE >> BensJiraConsole.log
+BensJiraConsole.exe PMPLAN_BURNUP >> BensJiraConsole.log
+BensJiraConsole.exe PMPLAN_RBURNUP >> BensJiraConsole.log
+BensJiraConsole.exe PMPLAN_STORIES >> BensJiraConsole.log
+BensJiraConsole.exe PMPLANS >> BensJiraConsole.log
+BensJiraConsole.exe WB_BURNUP >> BensJiraConsole.log
+BensJiraConsole.exe PMPLAN_NEW >> BensJiraConsole.log
+ECHO Finish time: %date% %time% >> BensJiraConsole.log
+ECHO Finish time: %date% %time% 
+pause
