9937317e269894cc0f69e49c22fb39b67eb1149c
Ben Rees <ben@rees.biz>
2025-09-17 13:02:12 +1200

    Change weekly batch file to write to console

--- WeeklyReports.bat
+++ WeeklyReports.bat
@@ -1,23 +1,12 @@
 cls
 @ECHO OFF
-ECHO ==================================================================================== >> BensJiraConsole.log
 ECHO Start time: %date% %time%
-ECHO Start time: %date% %time% >> BensJiraConsole.log
 cd \Development\BensJiraConsole\Bin\Debug\net9.0
-@ECHO ON
-@Echo BUG_STATS
-BensJiraConsole.exe BUG_STATS >> BensJiraConsole.log
-@Echo NOESTIMATE
-BensJiraConsole.exe NOESTIMATE >> BensJiraConsole.log
-@Echo PMPLAN_BURNUP
-BensJiraConsole.exe PMPLAN_BURNUPS >> BensJiraConsole.log
-@Echo PMPLAN_RBURNUP
-BensJiraConsole.exe PMPLAN_RBURNUP >> BensJiraConsole.log
-@Echo PMPLAN_STORIES
-BensJiraConsole.exe PMPLAN_STORIES >> BensJiraConsole.log
-@Echo PMPLANS
-BensJiraConsole.exe PMPLANS >> BensJiraConsole.log
-@ECHO OFF
-ECHO Finish time: %date% %time% >> BensJiraConsole.log
+BensJiraConsole.exe BUG_STATS 
+BensJiraConsole.exe NOESTIMATE
+BensJiraConsole.exe PMPLAN_BURNUPS 
+BensJiraConsole.exe PMPLAN_RBURNUP 
+BensJiraConsole.exe PMPLAN_STORIES 
+BensJiraConsole.exe PMPLANS 
 ECHO Finish time: %date% %time% 
 pause
