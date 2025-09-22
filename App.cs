a0f5f8813737c06093afd793f59660c6d75f1b8b
Ben Rees <ben@rees.biz>
2025-09-22 22:04:16 +1200

    DI and App init working

---
+++ App.cs
@@ -0,0 +1,78 @@
+﻿using System.Reflection;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BensJiraConsole;
+
+public class App(IEnumerable<IJiraExportTask> tasks)
+{
+    private readonly IJiraExportTask[] allTasks = tasks.ToArray();
+    private string[] commandLineArgs = [];
+
+    public static App Configure()
+    {
+        var builder = Host.CreateDefaultBuilder();
+        builder.ConfigureServices(services =>
+        {
+            services.AddSingleton<App>();
+            services.AddTransient<ICsvExporter, SimpleCsvExporter>();
+            services.AddTransient<IJiraQueryRunner, JiraQueryDynamicRunner>();
+            services.AddTransient<ICloudUploader, GoogleDriveUploader>();
+            services.AddTransient<IWorkSheetUpdater, GoogleSheetUpdater>();
+            services.AddTransient<IWorkSheetReader, GoogleSheetReader>();
+
+            // Find and Register all tasks
+            foreach (var taskType in TaskTypes())
+            {
+                services.AddSingleton(typeof(IJiraExportTask), taskType);
+            }
+        });
+
+        var host = builder.Build();
+        return host.Services.GetRequiredService<App>();
+    }
+
+    public async Task Run(string[] args)
+    {
+        this.commandLineArgs = args;
+        await ExecuteMode(args.Length > 0 ? args[0] : "NOT_SET");
+    }
+
+    private async Task ExecuteMode(string? mode)
+    {
+        IJiraExportTask? selectedTask = null;
+        StringBuilder help = new();
+        for (var index = 0; index < tasks.Count(); index++)
+        {
+            help.AppendLine($"{index + 1}: {this.allTasks[index].Description} ({this.allTasks[index].Key})");
+            if (mode == this.allTasks[index].Key || mode == (index + 1).ToString())
+            {
+                selectedTask = this.allTasks[index];
+            }
+        }
+
+        if (mode == "NOT_SET")
+        {
+            Console.WriteLine(help);
+            Console.WriteLine("Type 'exit' to quit.");
+            await Console.Out.FlushAsync();
+            mode = Console.ReadLine();
+            await ExecuteMode(mode);
+            return;
+        }
+
+        if (selectedTask is null)
+        {
+            return;
+        }
+
+        await selectedTask.ExecuteAsync(this.commandLineArgs);
+    }
+
+    private static IEnumerable<Type> TaskTypes()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        return assembly.GetTypes().Where(type => typeof(IJiraExportTask).IsAssignableFrom(type) && type is { IsInterface: false, IsAbstract: false });
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection;
-using System.Text;
-using BensJiraConsole;
+﻿using BensJiraConsole;
 
 public static class Program
 {
@@ -8,56 +6,11 @@ public static class Program
 
     public static async Task Main(string[] args)
     {
-        CommandLineArgs = args;
+        var app = App.Configure();
+
         Console.WriteLine("Jira Console Exporter tool.  Select a task to execute, or 'exit' to quit.");
-        var tasks = FindExportTaskImplementations();
-        await ExecuteMode(args.Length > 0 ? args[0] : "NOT_SET", tasks);
+        await app.Run(args);
+
         Console.WriteLine("Exiting.");
-    }
-
-
-    private static async Task ExecuteMode(string? mode, IJiraExportTask[] tasks)
-    {
-        IJiraExportTask? selectedTask = null;
-        StringBuilder help = new();
-        for (var index = 0; index < tasks.Count(); index++)
-        {
-            help.AppendLine($"{index + 1}: {tasks[index].Description} ({tasks[index].Key})");
-            if (mode == tasks[index].Key || mode == (index + 1).ToString())
-            {
-                selectedTask = tasks[index];
-            }
-        }
-
-        if (mode == "NOT_SET")
-        {
-            Console.WriteLine(help);
-            Console.WriteLine("Type 'exit' to quit.");
-            Console.Out.Flush();
-            mode = Console.ReadLine();
-            await ExecuteMode(mode, tasks);
-            return;
-        }
-
-        if (selectedTask is null)
-        {
-            return;
-        }
-
-        await selectedTask.ExecuteAsync(CommandLineArgs);
-    }
-
-    private static IJiraExportTask[] FindExportTaskImplementations()
-    {
-        var assembly = Assembly.GetExecutingAssembly();
-        var types = assembly.GetTypes()
-            .Where(type => typeof(IJiraExportTask).IsAssignableFrom(type)
-                           && type is { IsInterface: false, IsAbstract: false })
-            .ToList();
-        return types
-            .Select(Activator.CreateInstance)
-            .Cast<IJiraExportTask>()
-            .OrderBy(t => t.Key)
-            .ToArray();
     }
 }
