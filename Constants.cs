e31ae6bb5b1a15f8cf3bf1399517c9cf25341f4f
Ben <Ben@Rees.biz>
2025-11-01 11:35:03 +1300

    Officetech bugstats (#16)
    * Add officetech bug stats
    
    * Add Officetech to daily stats

--- Constants.cs
+++ Constants.cs
@@ -3,18 +3,20 @@
 public static class Constants
 {
     public const string ApplicationName = "BensJiraConsoleUploads";
-    public const string Envest = "Envest";
-    public const string EpicType = "Epic";
-    public const string StoryType = "Story";
     public const string BugType = "Bug";
-    public const string DoneStatus = "Done";
-    public const string HasDevTeamEstimate = "Dev Team Estimate";
-    public const string InDevStatus = "In Dev";
-    public const string InQaStatus = "In Test";
-    public const string SeverityCritical = "Critical";
-    public const string SeverityMajor = "Major";
-    public const string CodeFixLast6 = "Code Fix - Bug from recent development (past 6 months)";
     public const string BugTypeProduction = "Production";
     public const string BugTypeUat = "UAT";
+    public const string CodeFixLast6 = "Code Fix - Bug from recent development (past 6 months)";
+    public const string DoneStatus = "Done";
+    public const string Envest = "Envest";
+    public const string EpicType = "Epic";
+    public const string HasDevTeamEstimate = "Dev Team Estimate";
+    public const string JavPmJiraProjectKey = "JAVPM";
+    public const string InDevStatus = "In Dev";
+    public const string InQaStatus = "In Test";
+    public const string OtPmJiraProjectKey = "OTPM";
+    public const string SeverityCritical = "Critical";
+    public const string SeverityMajor = "Major";
+    public const string StoryType = "Story";
     public const string Unknown = "Unknown";
 }
