041177c87dcd67a392780036822dea7156be3d78
Ben Rees <ben@rees.biz>
2025-11-11 07:47:26 +1300

    ensure code is on master and up to date for daily run

--- WeeklyReports.bat
+++ WeeklyReports.bat
@@ -1,6 +1,10 @@
 cls
 @ECHO OFF
 ECHO Start time: %date% %time%
+cd \Development\BensJiraConsole
+git checkout master
+git pull
+dotnet build
 cd \Development\BensJiraConsole\Bin\Debug\net9.0
 BensJiraConsole.exe BUG_STATS
 BensJiraConsole.exe NOESTIMATE
