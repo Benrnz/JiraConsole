b7e0c2888227c37cc4a1b5e69828952d6f72b9fc
Ben Rees <ben@rees.biz>
2025-11-28 23:10:15 +1300

    Rearrange and add some better comments

--- Tasks/TeamVelocityCalculator.cs
+++ Tasks/TeamVelocityCalculator.cs
@@ -29,27 +29,31 @@ public class TeamVelocityCalculator(IJiraQueryRunner runner, ICsvExporter export
             foreach (var sprint in last5Sprints)
             {
                 var tickets = (await runner.SearchJiraIssuesWithJqlAsync(
-                        $"sprint = {sprint.Id} AND status = Done",
+                        $"sprint = {sprint.Id} AND status = Done", // this doesnt work because its the status as at now, not at sprint close time
                         [JiraFields.Severity, JiraFields.IssueType, JiraFields.StoryPoints, JiraFields.BugType]))
                     .Select(JiraIssue.CreateJiraIssueSlim)
                     .ToList();
 
                 exporter.SetFileNameMode(FileNameMode.ExactName, $"Velocity-TeamTickets-{team.TeamName}-{sprint.Id}");
                 exporter.Export(tickets);
+                // How many P1s did the team close in this sprint?
                 var p1s = tickets.Where(t => t is { IssueType: Constants.BugType, BugType: Constants.BugTypeProduction })
                     .Count(t => t.Severity == Constants.SeverityCritical);
                 totalP1Count += p1s;
-                var p2s = tickets.Where(t => t is { IssueType: Constants.BugType, BugType: Constants.BugTypeProduction })
-                    .Count(t => t.Severity == Constants.SeverityMajor);
-                totalP2Count += p2s;
-                totalOtherCount += tickets.Count(t => t is { IssueType: Constants.BugType }) - p1s - p2s;
-                totalStoryPoints += tickets.Sum(t => t.StoryPoints);
                 var p1StoryPoints = tickets.Where(t => t is { IssueType: Constants.BugType, BugType: Constants.BugTypeProduction, Severity: Constants.SeverityCritical })
                     .Sum(t => t.StoryPoints);
                 totalP1StoryPoints += p1StoryPoints;
+                // How many P2s did the team close in this sprint?
+                var p2s = tickets.Where(t => t is { IssueType: Constants.BugType, BugType: Constants.BugTypeProduction })
+                    .Count(t => t.Severity == Constants.SeverityMajor);
+                totalP2Count += p2s;
                 var p2StoryPoints = tickets.Where(t => t is { IssueType: Constants.BugType, BugType: Constants.BugTypeProduction, Severity: Constants.SeverityMajor })
                     .Sum(t => t.StoryPoints);
                 totalP2StoryPoints += p2StoryPoints;
+                // How many other bugs did the team close in this sprint?
+                totalOtherCount += tickets.Count(t => t is { IssueType: Constants.BugType }) - p1s - p2s;
+                // How many story points were *completed* in this sprint?
+                totalStoryPoints += tickets.Sum(t => t.StoryPoints);
                 totalOtherStoryPoints += tickets.Where(t => t.IssueType == Constants.BugType).Sum(t => t.StoryPoints) - p1StoryPoints - p2StoryPoints;
             }
 
