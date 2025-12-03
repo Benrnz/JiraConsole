d2f6d43c4e052c44f4b6866a48490517fd726d1f
Ben Rees <ben@rees.biz>
2025-12-03 14:32:18 +1300

    Allow the use of a filter with ALLTICKETS task

--- Tasks/ExportAllRecentJavPms.cs
+++ Tasks/ExportAllRecentJavPms.cs
@@ -4,7 +4,7 @@ namespace BensJiraConsole.Tasks;
 
 public class ExportAllRecentJavPms(IJiraQueryRunner runner, ICsvExporter exporter) : IJiraExportTask
 {
-    private const string KeyString = "ALLJAVPM";
+    private const string KeyString = "ALLTICKETS";
 
     /// <summary> Fields to include in the export: </summary>
     private static readonly IFieldMapping[] Fields =
@@ -31,12 +31,15 @@ public class ExportAllRecentJavPms(IJiraQueryRunner runner, ICsvExporter exporte
     ];
 
     public string Key => KeyString;
-    public string Description => "Export _all_JAVPM_ tickets from the last 18 months.";
+    public string Description => "Export JAVPM tickets from the last 18 months. Or optionally, specify a filter as an argument and export any tickets from any project.";
 
     public async Task ExecuteAsync(string[] args)
     {
         Console.WriteLine(Description);
-        var jql = "project=JAVPM AND created > -540d ORDER BY created"; //540 days = 18 months
+        var jql = args.Length > 1 ?
+            $"filter = {args[1]}"
+            : "project=JAVPM AND created > -540d ORDER BY created"; //540 days = 18 months
+
         Console.WriteLine(jql);
         var issues = await runner.SearchJiraIssuesWithJqlAsync(jql, Fields);
         Console.WriteLine($"{issues.Count} issues fetched.");
